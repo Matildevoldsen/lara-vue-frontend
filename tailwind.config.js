e0295c765cbaf9865fa6436635a2f22f887d5808
Matilde W. Enevoldsen <matildeene@outlook.com>
2021-04-08 15:37:49 +0100

    Configured tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  purge: [
+    './pages/**/*.vue',
+    './layouts/**/*.vue',
+    './components/**/*.vue',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/static/check.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  },
+}
